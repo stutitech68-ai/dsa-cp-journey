dc599816dab82cebebde36783e074cd908f06373
stutitech68-ai <stuti.tech68@gmail.com>
2026-08-20 22:06:04 +0530

    Update Brute_force_partial_output.cpp

--- Rearrange_Array_Elements_by_Sign/Brute_force_partial_output.cpp
+++ Rearrange_Array_Elements_by_Sign/Brute_force_partial_output.cpp
@@ -23,37 +23,63 @@
 // #include <bits/stdc++.h>
 // using namespace std;
 
- int main()
- {
-     int n;
-     cin >> n;
+ //didnt pass all test cases 
 
-     vector<int> arr(n);
-     for (int i = 0; i < n; i++) 
-     {
-         cin >> arr[i];
-     }
- int l=0,r=l+1;
- int temp=0;
-     while(l<arr.size())
-     {
-         if(arr[r]<0)
-         {
-             temp=arr[l+1];
-             arr[l+1]=arr[r];
-             arr[r]=temp;
-             l+=2;
-             break;
-         }
- 
-         r++;
-        
-      
-     }
-     for(int i=0;i<n;i++)
-     {
-         cout << arr[i]<<' ';
-     }
-     return 0;
- }
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    vector<int> nums(n);
+
+    cout << "Enter array elements: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> nums[i];
+    }
+
+    int l = 0, r = l + 1;
+    int temp = 0;
+
+    while (l < nums.size())
+    {
+        if (nums[l] < 0)
+        {
+            if (nums[r] >= 0)
+            {
+                temp = nums[l];
+                nums[l] = nums[r];
+                nums[r] = temp;
+            }
+        }
+
+        temp = 0;
+
+        if (nums[l] > 0 && nums[r] < 0)
+        {
+            temp = nums[l + 1];
+            nums[l + 1] = nums[r];
+            nums[r] = temp;
+
+            l += 2;
+        }
+
+        r++;
+    }
+
+    cout << "Rearranged array: ";
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << nums[i] << " ";
+    }
+
+    return 0;
+}
+
 
